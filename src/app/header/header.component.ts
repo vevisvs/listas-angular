dd4af74c74fb17c9f357060a076e335b59088d09
vevis <vevisvs@gmail.com>
2023-06-29 23:18:14 -0300

    creacion de componentes header y main, implementacion de estilo basico

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <h1>
+      Listas en Angular
+    </h1>
+  `,
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+}
